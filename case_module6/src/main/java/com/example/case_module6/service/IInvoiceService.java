c63f4c0b42ce51ca83c9486c914803b2b4078ff8
nthanhquoc60 <nthanhquoc60@gmail.com>
2025-02-18 22:23:45 +0700

    x

--- case_module6/src/main/java/com/example/case_module6/service/IInvoiceService.java
+++ case_module6/src/main/java/com/example/case_module6/service/IInvoiceService.java
@@ -1,6 +1,11 @@
 package com.example.case_module6.service;
 
+import com.example.case_module6.model.Cart;
 import com.example.case_module6.model.Invoice;
 
-public interface IInvoiceService extends IService<Invoice, Long> {
+import java.util.Optional;
+
+public interface IInvoiceService {
+    Invoice createInvoice(Invoice invoice);
+    Optional<Cart> assignInvoiceToCart(Long cartId, Long invoiceId);
 }
--- case_module6/src/main/java/com/example/case_module6/service/implement/FeedBackService.java
+++ case_module6/src/main/java/com/example/case_module6/service/implement/FeedBackService.java
@@ -1,9 +1,13 @@
 package com.example.case_module6.service.implement;
 
+import com.example.case_module6.model.Cart;
 import com.example.case_module6.model.Feedback;
+import com.example.case_module6.model.Notification;
 import com.example.case_module6.repository.FeedBackRepository;
 import com.example.case_module6.service.IFeedbackService;
+import com.example.case_module6.service.INotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -14,10 +18,22 @@ public class FeedBackService implements IFeedbackService {
     @Autowired
     private FeedBackRepository feedBackRepository;
 
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    private INotificationService notificationService;
+
     @Override
     public Feedback createFeedback(Feedback feedback) {
-        feedback.setDateFeedback(java.time.LocalDateTime.now());
-        return feedBackRepository.save(feedback);
+        feedback.setDateFeedback(LocalDateTime.now());
+        Feedback savedFeedback = feedBackRepository.save(feedback);
+        Notification notification = new Notification();
+        notification.setContent("Có Feed Back Mới : # " + savedFeedback.getId());
+        notification.setDateNote(LocalDateTime.now());
+        notificationService.save(notification);
+        messagingTemplate.convertAndSend("/topic/notifications", notification);
+        return savedFeedback;
     }
 
     @Override
