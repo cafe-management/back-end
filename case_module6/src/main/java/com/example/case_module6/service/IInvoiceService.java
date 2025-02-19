05e7e62d5333c5610fea35cc13a8f667542970f6
nthanhquoc60 <nthanhquoc60@gmail.com>
2025-02-19 15:28:45 +0700

    quoc

--- case_module6/src/main/java/com/example/case_module6/service/IInvoiceService.java
+++ case_module6/src/main/java/com/example/case_module6/service/IInvoiceService.java
@@ -3,9 +3,11 @@ package com.example.case_module6.service;
 import com.example.case_module6.model.Cart;
 import com.example.case_module6.model.Invoice;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IInvoiceService {
+    List<Invoice> getAllInvoice();
     Invoice createInvoice(Invoice invoice);
     Optional<Cart> assignInvoiceToCart(Long cartId, Long invoiceId);
 }
