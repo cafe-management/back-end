fb445b5f78ba7ea569d7463079a35a8a69caf9ea
nthanhquoc60 <nthanhquoc60@gmail.com>
2025-02-21 16:25:30 +0700

    quoc

--- case_module6/src/main/java/com/example/case_module6/repository/FeedBackRepository.java
+++ case_module6/src/main/java/com/example/case_module6/repository/FeedBackRepository.java
@@ -2,11 +2,14 @@ package com.example.case_module6.repository;
 
 
 import com.example.case_module6.model.Feedback;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface FeedBackRepository extends JpaRepository<Feedback,Long> {
-    List<Feedback>findByDateFeedbackBetween(LocalDateTime start, LocalDateTime end);
+    Page<Feedback> findAll(Pageable pageable);
+    Page<Feedback> findByDateFeedbackBetween(LocalDateTime start, LocalDateTime end, Pageable pageable);
 }
--- case_module6/src/main/java/com/example/case_module6/service/IFeedbackService.java
+++ case_module6/src/main/java/com/example/case_module6/service/IFeedbackService.java
@@ -1,13 +1,15 @@
 package com.example.case_module6.service;
 
 import com.example.case_module6.model.Feedback;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface IFeedbackService {
     Feedback createFeedback(Feedback feedback);
-    List<Feedback> getAllFeedback();
+    Page<Feedback> getAllFeedback(Pageable pageable);
     Feedback getFeedbackById(Long id);
-    List<Feedback> getFeedbacksByDate(LocalDateTime start, LocalDateTime end);
+    Page<Feedback> getFeedbacksByDate(LocalDateTime start, LocalDateTime end, Pageable pageable);
 }
